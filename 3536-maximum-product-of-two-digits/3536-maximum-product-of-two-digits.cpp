3c5f78436b18f1ad1b797678523af5e2a37d2701
Harshvardhan Dhere <124353465+Harshvardhan770@users.noreply.github.com>
2026-07-25 20:55:14 +0530

    Time: 1 ms (23.15%), Space: 8.9 MB (54.99%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -11,27 +11,8 @@ public:
             x.push_back(last);
             n/=10;
         }
-
-        for(auto i : x)
-        {
-            if(i>max)
-            {
-                max = i;
-            }
-        }
-
-        sort(x.begin(),x.end());
-        x.pop_back();
-        for(auto i : x)
-        {
-            if(i>secMax)
-            {
-                secMax = i;
-            }
-        }
-
-        return max * secMax;
-
-
+        
+        sort(x.begin(),x.end(),greater<int>());
+        return x[0] * x[1];
     }
 };
